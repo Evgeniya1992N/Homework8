a6ef04e3ee489505415b7a085318e6d088efe51b
Evgeniya <eva.goddess@mail.ru>
2023-08-15 11:50:37 +0200

    commit hw 8

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] weights = new int[12];
+        weights[0] = 90;
+        int januaryWeight = weights[0];
+        System.out.println(januaryWeight);
+        System.out.println(weights[0]);
     }
 }
